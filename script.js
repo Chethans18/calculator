89a78b9537f791bfa84a50e7ee4001c4c3f7ae47
Chethan S <chethans17jan2004@gmail.com>
2023-11-22 10:06:51 +0530

    created html,css,and javascript files and added basic structure of calculator

---
+++ script.js
@@ -0,0 +1,90 @@
+let firstNumber = 0, secondNumber = 0, operator;
+
+
+//this display value of button pressed into the input 
+
+function display(value) {
+    let display = document.querySelector('#display')
+    display.value += value
+}
+//function to clear the values in the display
+const clear = document.querySelector('#clear')
+clear.addEventListener('click', () => {
+    const display = document.querySelector('#display');
+    display.value = "";
+});
+
+const one = document.querySelector('#one');
+one.addEventListener('click', () => {
+    display('1');
+});
+const two = document.querySelector('#two');
+two.addEventListener('click', () => {
+    display('2');
+
+});
+const three = document.querySelector('#three');
+three.addEventListener('click', () => {
+    display('3');
+
+});
+const four = document.querySelector('#four');
+four.addEventListener('click', () => {
+    display('4');
+
+});
+const five = document.querySelector('#five');
+five.addEventListener('click', () => {
+    display('5');
+});
+const six = document.querySelector('#six');
+six.addEventListener('click', () => {
+    display('6');
+
+});
+const seven = document.querySelector('#seven');
+seven.addEventListener('click', () => {
+    display('7');
+
+});
+const eight = document.querySelector('#eight');
+eight.addEventListener('click', () => {
+    display('8');
+
+});
+const nine = document.querySelector('#nine');
+nine.addEventListener('click', () => {
+    display('9');
+
+});
+const zero = document.querySelector('#zero');
+zero.addEventListener('click', () => {
+    display('0');
+
+});
+const dot = document.querySelector('#dot');
+dot.addEventListener('click', () => {
+    display('.');
+
+});
+const add = document.querySelector('#add');
+add.addEventListener('click', () => {
+    display('+');
+
+});
+const sub = document.querySelector('#sub');
+sub.addEventListener('click', () => {
+    display('-');
+
+});
+const multiply = document.querySelector('#multiply');
+multiply.addEventListener('click', () => {
+    display('X');
+
+});
+const divide = document.querySelector('#divide');
+divide.addEventListener('click', () => {
+    display('÷');
+
+});
+
